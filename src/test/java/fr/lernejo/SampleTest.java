47a9f00fc0274adc41496347794797303e12721d
chheikhlo <chheikhlo@gmail.com>
2022-10-15 01:41:03 +0200

    coverage > 85%

--- src/test/java/fr/lernejo/SampleTest.java
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -15,4 +15,33 @@ class SampleTest {
             .as("Addition of 2 and 2")
             .isEqualTo(4);
     }
+
+    @Test
+    void mult_2_and_3_should_return_6() {
+        int result = sample.op(Sample.Operation.MULT, 2, 3);
+
+        Assertions.assertThat(result)
+            .as("Multiplication of 2 and 3")
+            .isEqualTo(6);
+    }
+
+    @Test
+    void fact_4_return_24() {
+        int result = sample.fact( 4);
+
+        Assertions.assertThat(result)
+            .as("Factorielle de 4")
+            .isEqualTo(24);
+    }
+
+    @Test
+    void fact_return_int() {
+        int result = sample.fact( 4);
+
+        boolean isInt = (result>=0);
+
+        Assertions.assertThat(isInt)
+            .as("est il un entier")
+            .isEqualTo(true);
+    }
 }
